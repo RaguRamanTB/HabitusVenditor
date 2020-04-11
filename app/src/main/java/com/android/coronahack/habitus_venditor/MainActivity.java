da261a7f9d4b2382ed968947608682b8f5b903a9
RaguRamanTB <aakash1956ragu@gmail.com>
2020-04-11 20:07:46 +0530

    Initial commit

---
+++ app/src/main/java/com/android/coronahack/habitus_venditor/MainActivity.java
@@ -0,0 +1,19 @@
+package com.android.coronahack.habitus_venditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean firstStart = sharedPreferences.getBoolean("firstStart", true);
+        
+    }
+}
